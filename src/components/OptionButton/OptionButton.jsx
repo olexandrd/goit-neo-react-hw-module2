d842c1f83932b394844191d7b25e8d91202a1c61
Olexandrd <olexandrd@gmail.com>
2024-07-30 21:37:53 +0300

    options refactoring

--- src/components/OptionButton/OptionButton.jsx
+++ src/components/OptionButton/OptionButton.jsx
@@ -2,7 +2,10 @@ import css from "./OptionButton.module.css";
 
 const OptionButton = ({ name, clickHandler }) => {
   return (
-    <button className={css.button} onClick={clickHandler}>
+    <button
+      className={css.button}
+      onClick={() => clickHandler(name.toLowerCase())}
+    >
       {name}
     </button>
   );
--- src/components/Options/Options.jsx
+++ src/components/Options/Options.jsx
@@ -4,12 +4,9 @@ import css from "./Options.module.css";
 const Options = ({ feedbacks, clickHandler, resetHandler }) => {
   return (
     <div className={css.optionsWrapper}>
-      <OptionButton clickHandler={() => clickHandler("good")} name="Good" />
-      <OptionButton
-        clickHandler={() => clickHandler("neutral")}
-        name="Neutral"
-      />
-      <OptionButton clickHandler={() => clickHandler("bad")} name="Bad" />
+      <OptionButton clickHandler={clickHandler} name="Good" />
+      <OptionButton clickHandler={clickHandler} name="Neutral" />
+      <OptionButton clickHandler={clickHandler} name="Bad" />
 
       {feedbacks > 0 ? (
         <OptionButton clickHandler={resetHandler} name="Reset" />
